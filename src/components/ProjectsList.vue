7d58e07eac319e1f5462a58acdf178326fcca51f
Fábio Sousa <fabiodaniel.2000@gmail.com>
2026-03-20 17:02:04 +0000

    Bug fix

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -108,6 +108,8 @@ export default Vue.extend({
   transition: box-shadow 0.3s ease, transform 0.3s ease;
   min-height: 380px;
   position: relative;
+  transform: translateZ(0);
+  -webkit-transform: translateZ(0);
 }
 
 .featured-card:hover {
